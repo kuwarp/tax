e1a7bc8f4aa401b31ea0404878ca252650a45e18
kuwarp <guru75000@gmail.com>
2021-12-26 10:11:48 +0000

    csswork

---
+++ src/screens/sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Sidebar() {
+    return(
+        <div className="Sidebar-fixed">
+            <a href="*" className="logo bl">
+                <img src={logo} className="bl"/>
+                </a>
+            </div>
+    )
+    
+    }
+   export default Sidebar
